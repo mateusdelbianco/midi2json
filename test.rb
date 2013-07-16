7ac09b3b6f3be7ab6d14ac8aaabad1f3675c9325
Mateus Del Bianco <mateus@delbianco.net.br>
2013-07-16 01:20:59 -0300

    de-duplicate lyrics

--- test.rb
+++ test.rb
@@ -5,11 +5,19 @@ require "json"
 
 class LyricSyllable < OpenStruct
   def as_json
-    {
+    hash = {
       "start" => self.seq.pulses_to_seconds(self.start.to_f).round(3),
       "duration" => self.seq.pulses_to_seconds(self.duration.to_f).round(3),
       "text" => self.text
     }
+    unless start2.nil?
+      hash.merge!("start2" => self.seq.pulses_to_seconds(self.start2.to_f).round(3))
+    end
+    hash
+  end
+
+  def similar_to?(another)
+    self.duration == another.duration && self.text == another.text
   end
 end
 
@@ -58,6 +66,28 @@ lyrics_track.each do |event|
     :duration => durations[event.time_from_start],
     :text => event.data.collect{|x| x.chr(Encoding::UTF_8)}.join,
   )
+end
+
+def half_equal lyrics_syllables
+  half = lyrics_syllables.count / 2
+  (0..(half-1)).each do |x|
+    unless lyrics_syllables[x].similar_to?(lyrics_syllables[x + half])
+      return false
+    end
+  end
+  return true
+end
+
+def half_lyrics lyrics_syllables
+  half = lyrics_syllables.count / 2
+  (0..(half-1)).collect do |x|
+    lyrics_syllables[x].start2 = lyrics_syllables[x + half].start
+    lyrics_syllables[x]
+  end
+end
+
+if half_equal(lyrics_syllables)
+  lyrics_syllables = half_lyrics(lyrics_syllables)
 end
 
 puts lyrics_syllables.collect(&:as_json).to_json
