780a24ba4712f602a06227908fdfeac4ff35a693
Mateus Del Bianco <mateus@delbianco.net.br>
2013-07-16 01:50:34 -0300

    fix second syllable timing

--- json2html.rb
+++ json2html.rb
@@ -10,6 +10,6 @@ lyrics_syllables = JSON::load(File.read(ARGV[0]))
 
 lyrics_syllables.each do |ls|
   print "<span data-dur=\"#{ls['duration']}\" data-begin=\"#{ls['start']}\""
-  print " data-begintwo=\"#{ls['start2']}\"" unless ls['start2']
+  print " data-begintwo=\"#{ls['start2']}\"" unless ls['start2'].nil?
   print ">#{ls['text'].gsub("\n", "<br />").gsub("\r", "<br />")}</span>"
 end
